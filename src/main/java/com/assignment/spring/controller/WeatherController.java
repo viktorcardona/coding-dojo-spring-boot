85369fdfcae8926acf1a772652415def1d10866b
Victor Cardona <victorcardona@Victors-MacBook-Air.local>
2021-02-14 17:07:35 +0100

    Removing unused import

--- src/main/java/com/assignment/spring/controller/WeatherController.java
+++ src/main/java/com/assignment/spring/controller/WeatherController.java
@@ -11,7 +11,6 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
